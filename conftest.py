66edccf722289370e0bcde665b9ebdccf724f0a4
heirenweixiao <heirenweixiao66@163.com>
2025-12-30 09:14:15 +0800

    DB校验

--- conftest.py
+++ conftest.py
@@ -16,13 +16,7 @@ def client():
 @pytest.fixture(scope="session")
 def db_conn():
     conn = pymysql.connect(
-        host='192.168.1.151',
-        port=3306,
-        user='zhoujiman@mop#mop',
-        password='reabam123@mop',
-        database='rb_ts_core',
-        charset='utf8mb4',
-        cursorclass=pymysql.cursors.DictCursor
-    )
+
+   )
     yield conn
     conn.close()
